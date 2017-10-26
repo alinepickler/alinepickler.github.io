2251cd3f5be2ca396f9a7fcb564f0556b1ddd684
Aline Pickler <aline.pickler@gmail.com>
2017-10-27 00:42:33 +0200

    Add X animation to mobile button

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,3 +3,7 @@ document.getElementById("menu-mobile-btn").onclick = toggleMobileMenu;
 function toggleMobileMenu() {
   document.getElementById("nav-mobile").classList.toggle("show");
 }
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
